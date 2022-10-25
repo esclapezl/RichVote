fa1b2e3970da5f1cca953c819cfdbe5d8b7cdc0f
nathan souvignet <nathan.souvignet@etu.umontpellier.fr>
2022-10-25 16:52:24 +0200

    squelette

--- web/frontController.php
+++ web/frontController.php
@@ -1,4 +1,19 @@
 <?php
-yoyyo
+namespace App\web;
+use App\Controller\Controller as Controller;
 
+require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
+
+// instantiate the loader
+$loader = new \App\Lib\Psr4AutoloaderClass();
+// register the base directories for the namespace prefix
+$loader->addNamespace('App', __DIR__ . '/../src');
+// register the autoloader
+$loader->register();
+
+$action = 'readAll';
+if(isset($_GET['action'])) {
+    $action = $_GET['action'];
+}
+Controller::$action(); // Appel de la méthode statique $action de ControllerVoiture
 ?>
