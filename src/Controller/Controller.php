8b48a42eeaa15b2cdfdca445374eff3560581a36
nathan souvignet <nathan.souvignet@etu.umontpellier.fr>
2022-10-28 11:39:21 +0200

    connexion a oracle ok sur session iut

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,13 +4,7 @@ use App\Model\Repository\DatabaseConnection as DataBaseConnection;
 use mysql_xdevapi\DatabaseObject;
 
 class Controller{
-    public static function test()
-    {
-        echo "ah";
-    }
-
     public static function readAll(){
-        echo "read All";
         $pdo = DataBaseConnection::getInstance()::getPdo();
 
         $sql = "SELECT * FROM testRepSec";
@@ -18,15 +12,15 @@ class Controller{
         $pdoStatement = $pdo->prepare($sql);
 
         $pdoStatement->execute();
-        $var = $pdoStatement->fetch();
 
         $idPrevQ = 0;
-        foreach ($var as $currentQuestion){
-            if($idPrevQ != $currentQuestion[idQues])
-            echo "id Question: $currentQuestion[idQuestion]"
+        foreach ($pdoStatement as $currentQuestion){
+            if($idPrevQ != $currentQuestion['idQuestion']){
+                $idPrevQ = $currentQuestion['idQuestion'];
+                echo "autre question: $currentQuestion[intituleQuestion]<br>";
+            }
+            echo "Section: $currentQuestion[idSection] intitule :$currentQuestion[intituleSection] $currentQuestion[descriptionSection] <br>";
         }
-
-        echo "je test, l'id de la question: $var[idQuestion], sa section: $var[idSection]";
     }
 }
 ?>
