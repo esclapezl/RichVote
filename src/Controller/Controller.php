6e73c638f9c901bfdda4870d11a8b3c70168a024
nathan souvignet <nathan.souvignet@etu.umontpellier.fr>
2022-10-28 10:42:47 +0200

    test git

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -11,7 +11,22 @@ class Controller{
 
     public static function readAll(){
         echo "read All";
-        DataBaseConnection::getInstance()::getPdo();
+        $pdo = DataBaseConnection::getInstance()::getPdo();
+
+        $sql = "SELECT * FROM testRepSec";
+
+        $pdoStatement = $pdo->prepare($sql);
+
+        $pdoStatement->execute();
+        $var = $pdoStatement->fetch();
+
+        $idPrevQ = 0;
+        foreach ($var as $currentQuestion){
+            if($idPrevQ != $currentQuestion[idQues])
+            echo "id Question: $currentQuestion[idQuestion]"
+        }
+
+        echo "je test, l'id de la question: $var[idQuestion], sa section: $var[idSection]";
     }
 }
 ?>
