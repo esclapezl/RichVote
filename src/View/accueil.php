f0debf1bc46710667a06d9510ad11f7eb0168b57
Odilon VIDAL <odilon.vidal@etu.umontpellier.fr>
2022-11-05 22:47:57 +0100

    ajout animation accueil

--- src/View/accueil.php
+++ src/View/accueil.php
@@ -1,54 +1,25 @@
 
 <header>
     <div class="scroll"> <span>scroll &#10132</span></div>
+    <div class="blocvid">
+    <video autoplay="autoplay" muted="" loop="infinite" src="img/trailer.webm"></video>
+    </div>
 </header>
 <div class="blockAccueil">
-    <div class="contain">
-        <div class="box">
-            <span style="--i:1"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:2"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:3"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:4"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:5"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:6"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:7"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:8"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:9"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:10"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:11"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:12"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:13"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:14"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:15"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
-            <span style="--i:16"><i>BIENVENUE</i> CHEZ <i>RICHVOTE </i>!</span>
 
-
-        </div>
-    </div>
-    <div class="text-container">
         <div class="text-box">
-            <h3>Connu sous le nom d'ODIV</h3>
+            <h3>Qu'est ce que RichVote ?</h3>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                eius molestiae perferendis eos provident vitae iste. Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quam blanditiis,
-                officiis, voluptate quisquam aperiam nobis est, quis neque esse
-                soluta dolores iste sit corrupti! Magnam, delectus qui. Odit,
-                accusamus doloremque.
-            </p><br>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex est
-                rerum id quis eaque rem suscipit reprehenderit quaerat vero
-                nesciunt?
+                RichVote est un site de vote. Il possède le système de vote le plus élaborée et le plus
+                sûr que l'on peut retrouver sur le web.
             </p>
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Voluptatum, eaque voluptatibus. Eaque tempore quam nesciunt. Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Eum adipisci
-                blanditiis assumenda atque officiis? Vel, fuga facilis quia
-                reprehenderit nihil optio harum. Quo sapiente veniam atque
-                corporis tempora non repellat?
+                Suivez vos différents sujets en vous connectant <a href="#">ici</a>.
+            </p>
+            <p>
+                Si vous n'êtes pas inscrit, ne tardez plus !
+                Vous pouvez être nommé à différents rôles, vous avez juste à cliquer <a href="#">ici</a>.
             </p>
         </div>
-    </div>
+
 </div>
