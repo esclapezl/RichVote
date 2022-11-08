e45e3ac7cdf3a10acfecc4f9d5e9e6169f873ad1
Odilon VIDAL <odilon.vidal@etu.umontpellier.fr>
2022-11-08 16:48:24 +0100

    modifier

--- src/View/question/deleted.php
+++ src/View/question/deleted.php
@@ -2,11 +2,9 @@
 use App\Model\DataObject\Question;
 /** @var Question $question*/
 ?>
-<div class="block">
-    <div class="text-box">
-        <p> La question intitulée <?=$question->getIntitule()?> a bien été supprimée.</p>;
-    </div>
-</div>
+
+        <p> La question intitulée <?=$question->getIntitule()?> a bien été supprimée.</p>
+
 <?php
 require_once __DIR__ . "/../question/list.php";
 ?>
