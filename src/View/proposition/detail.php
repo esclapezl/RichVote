fc2130a0f253e6c5cd465b0c696be693d9102235
nathan <nath.souvignet@gmail.com>
2022-11-09 01:15:26 +0100

    ajout intitule proposition

--- src/View/proposition/detail.php
+++ src/View/proposition/detail.php
@@ -4,7 +4,7 @@ use App\Model\DataObject\Proposition;
 ?>
 <div class="block">
     <div class="text-box">
-        <div class="ligneExt"> <h1><?=$proposition->getIdProposition()?> Proposition titre</h1> <h3>Détail de la proposition</h3></div>
+        <div class="ligneExt"> <h1><?=$proposition->getIntitule()?> Proposition titre</h1> <h3>Détail de la proposition</h3></div>
         <div class="ligneExt"><div class="ligne"></div><div class="ligne"></div></div>
         <div class="ligneExt">
             <?php
