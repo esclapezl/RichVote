5a3f0f29538e058741268a965fb37a20827b47a5
Odilon <odilon.vidal@etu.umontpellier.fr>
2022-11-09 01:02:09 +0100

    l'entiereté des interfaces v1.0

--- src/View/proposition/detail.php
+++ src/View/proposition/detail.php
@@ -4,19 +4,20 @@ use App\Model\DataObject\Proposition;
 ?>
 <div class="block">
     <div class="text-box">
-        <div class="ligneExt"> <h1><!--=$proposition->getIntitule()?-->Proposition titre</h1> <h3>Détail de la proposition</h3></div>
+        <div class="ligneExt"> <h1><?=$proposition->getIdProposition()?> Proposition titre</h1> <h3>Détail de la proposition</h3></div>
         <div class="ligneExt"><div class="ligne"></div><div class="ligne"></div></div>
         <div class="ligneExt">
             <?php
-            echo '<a href=frontController.php?controller=proposition&action=update&id=' . rawurlencode($proposition->getIdProposition()) . ' class="optQuestion">modifier</a>' .
-                '<a href=frontController.php?controller=proposition&action=delete&id='. rawurlencode($proposition->getIdProposition()) . ' class="optQuestion">supprimer</a>';
+            echo '<a href=frontController.php?controller=proposition&action=update&id=' . rawurlencode($proposition->getIdProposition()) . ' class="optQuestion">Modifier</a>' .
+                '<a href=frontController.php?controller=proposition&action=delete&id='. rawurlencode($proposition->getIdProposition()) . ' class="optQuestion">Supprimer</a>';
             ?>
         </div>
-        <p>
-        <h1><?=$proposition->getIdProposition()?></h1>
-        </p>
-        <?php foreach ($proposition->getSectionsTexte() as $texte) {
-            echo "<p>" . $texte . "</p>";
+
+        <?php
+        foreach ($proposition->getSectionsTexte() as $texte) {
+            echo '<p><h3>Section</h3><textarea type="text" rows="4" cols="100">' . $texte . '</textarea></p>';
         }?>
+
+
     </div>
 </div>
