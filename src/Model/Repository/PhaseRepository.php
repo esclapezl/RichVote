33a9947e84396220604af774bb5d4e8cca5991b4
nathan <nath.souvignet@gmail.com>
2022-11-24 00:50:24 +0100

    faudra ptetre l'annuler

---
+++ src/Model/Repository/PhaseRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model\Repository;
+
+use App\Model\DataObject\AbstractDataObject;
+use App\Model\DataObject\Phase;
+
+class PhaseRepository extends AbstractRepository
+{
+    protected function getNomTable(): string
+    {
+        return 'Phases';
+    }
+
+    protected function getNomClePrimaire(): string
+    {
+        return 'idPhase';
+    }
+
+    protected function getNomsColonnes(): array
+    {
+        return ['idPhase',
+                'dateDebut',
+                'dateFin',
+                'typePhase'];
+    }
+
+    protected function construire(array $objetFormatTableau): AbstractDataObject
+    {
+        return new Phase($objetFormatTableau['idPhase'],
+                        $objetFormatTableau['dateDebut'],
+                        $objetFormatTableau['dateFin'],
+                        $objetFormatTableau['typePhase']);
+    }
+
+    public function getCurrentTypePhase(string $idQuestion){
+        $sql = 'SELECT * FROM PHASES WHERE idQuestionConcerne=:idQuestion AND dateDebut<=SYSDATE AND dateFin>SYSDATE';
+        $pdo = DatabaseConnection::getInstance()::getPdo();
+        $statement = $pdo->prepare($sql);
+        $statement->execute(['idQuestion' => $idQuestion]);
+        $type = $statement->fetch()['TYPEPHASE'];
+        if(!$type){
+            return 'consultation';
+        }
+        else{
+            return $type;
+        }
+    }
+}
