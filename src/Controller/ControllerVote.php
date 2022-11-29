14163f6bc52fa067a282b60e4e7a265f799f96d9
nathan <nath.souvignet@gmail.com>
2022-11-29 20:20:58 +0100

    base pour voter en scrutinMajoritaire

--- src/Controller/ControllerVote.php
+++ src/Controller/ControllerVote.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Lib\ConnexionUtilisateur;
+use App\Lib\MessageFlash;
+use App\Model\HTTP\Session;
 use App\Model\Repository\PropositionRepository;
 use App\Model\Repository\QuestionRepository;
 use App\Model\Repository\UserRepository;
 
 class ControllerVote extends GenericController
 {
-    public static function scrutinMajoritaire() : void
+    public static function scrutinMajoritaire() : void{
+        $parametres = array(
+            'pagetitle' => 'Scrutin Majoritaire',
+            'cheminVueBody' => 'vote/scrutinMajoritaire.php',
+        );
+        self::afficheVue('view.php', $parametres);
+    }
+
+    public static function voterScrutinMajoritaire() : void
     {
         $question = (new QuestionRepository())->select($_GET['idQuestion']);
         $propositions = (new PropositionRepository())->selectAllForQuestion($question->getId());
         $parametres = array(
             'pagetitle' => 'Scrutin Majoritaire',
-            'cheminVueBody' => 'vote/scrutinMajoritaire.php',
+            'cheminVueBody' => 'vote/voter/scrutinMajoritaire.php',
             'question' => $question,
             'propositions' => $propositions
         );
@@ -22,12 +33,20 @@ class ControllerVote extends GenericController
     }
 
     public static function scrutinMajoritaireVoted(){
-        $proposition = (new PropositionRepository())->select($_GET['idProposition']);
-        $user = (new UserRepository())->select($_GET['idUser']);
-        $scoreVote = $_GET['score'];
+        $proposition = (new PropositionRepository())->select($_POST['idProposition']);
+        $user = ConnexionUtilisateur::getLoginUtilisateurConnecte();
+        if($user == null){
+            MessageFlash::ajouter('danger', 'vote refusé, vous n\'êtes pas connecté');
+        }
+        else{
+            $scoreVote = 1;
 
-        var_dump($proposition->getId() . "  " . $user->getId() . "  " . $scoreVote);
-        (new PropositionRepository())->voter($proposition, $user, $scoreVote);
+            (new PropositionRepository())->voter($proposition, $user, $scoreVote);
+
+            MessageFlash::ajouter('success', 'Vous avez voté !');
+        }
+
+        ControllerQuestion::readAll();
     }
     public static function consultation() : void
     {
