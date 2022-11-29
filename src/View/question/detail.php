1df07356626aded19bd63e0a1b4d9da960b0207d
nathan <nath.souvignet@gmail.com>
2022-11-29 22:39:00 +0100

    vote  dans Question vue

--- src/View/question/detail.php
+++ src/View/question/detail.php
@@ -16,11 +16,25 @@ switch ($typePhase) {
 }
 ?>
 
+
+<?php
+use \App\Lib\ConnexionUtilisateur;
+use App\Model\Repository\VoteRepository;
+$link = "frontController.php?controller=vote&action=" . ucfirst(rawurlencode($question->getCurrentPhase()->getType()));
+if(ConnexionUtilisateur::estConnecte()){
+    $idUser = ConnexionUtilisateur::getLoginUtilisateurConnecte();
+    if(VoteRepository::peutVoter($idUser, $question->getId())) {
+        $link = 'frontController.php?controller=vote&action=voterScrutinMajoritaire&idQuestion=' . $question->getId();
+    }
+}
+?>
+
+
 <div class="block">
     <div class="text-box">
         <div class="ligneExt"> <div><a class="optQuestion" href=frontController.php?controller=question&action=readAll>↩</a>
                 <h1><?=htmlspecialchars($question->getIntitule())?></h1></div> <div><h3>Détail de la question</h3><div class="ligneAlign">
-                    <a href=frontController.php?controller=vote&action=<?= ucfirst(rawurlencode($question->getCurrentPhase()->getType()))?>>
+                    <a href=<?=$link?>>
 
                         <h2><?=$typePhase?></h2></a></div></div></div>
         <div class="ligneExt"><div class="ligne"></div><div class="ligne"></div></div>
@@ -28,18 +42,7 @@ switch ($typePhase) {
             echo
                 '<a class="optQuestion" href=frontController.php?controller=proposition&action=readAll&id=' . rawurlencode($question->getId()) . '>Liste des propositions</a>'
                 .
-                '<a class="optQuestion" href=frontController.php?controller=question&action=delete&id='. rawurlencode($question->getId()) . '>Supprimer</a>';
-
-use \App\Lib\ConnexionUtilisateur;
-use App\Model\Repository\VoteRepository;
-if(ConnexionUtilisateur::estConnecte()){
-    $idUser = ConnexionUtilisateur::getLoginUtilisateurConnecte();
-    if(VoteRepository::peutVoter($idUser, $question->getId())) {
-        echo '<a href="frontController.php?controller=vote&action=voterScrutinMajoritaire&idQuestion=' . $question->getId() . '">voter</a>';
-    }
-}
-
-            ?>
+                '<a class="optQuestion" href=frontController.php?controller=question&action=delete&id='. rawurlencode($question->getId()) . '>Supprimer</a>'; ?>
         </div>
         <div class="ligneExt"><?php echo '<a class="optQuestion" href=frontController.php?controller=proposition&action=create&id=' . rawurlencode($question->getId()) . '>Créer proposition</a>' .
                 '<a class="optQuestion" href=frontController.php?controller=question&action=update&id=' . rawurlencode($question->getId()) . '>Modifier</a>';
