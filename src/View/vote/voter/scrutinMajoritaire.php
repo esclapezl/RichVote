ff9d31c4ef85eaf585add04c2dc4dc9d89069e24
Odilon <odilon.vidal@etu.umontpellier.fr>
2022-11-30 01:33:11 +0100

    voter

--- src/View/vote/voter/scrutinMajoritaire.php
+++ src/View/vote/voter/scrutinMajoritaire.php
@@ -1,14 +1,32 @@
 <div class="block">
+    <div class="text-box">
     <form method="post" action="frontController.php?controller=vote&action=scrutinMajoritaireVoted">
+        <div class="ligneCent"> <h1>Vous pouvez voter.</h1></div>
+
+        <div class="ligneCent"><div class="ligne"></div></div><br>
+        <div class="ligneCent"> <h3>Votre choix restera confidentiel.</h3></div>
+        <div class="descG"></div>
+
+
         <?php
         /** @var Proposition[] $propositions */
         foreach ($propositions as $proposition){
             $idProposition = $proposition->getId();
             $intituleProposition = $proposition->getIntitule();
-            echo"<input type='radio' id='p$idProposition' name='idProposition' value='$idProposition'> $intituleProposition </input>";
+            echo '<div class="ligneCent">
+
+                    
+                    <h3>'.ucfirst($intituleProposition).' → </h3>
+                    <label for="checkbox"  class="checkbox"><input type="radio" id='.$idProposition .' name='.$intituleProposition .'></label>
+                    </div><div class="descP"></div>';
         }
 
         ?>
-        <button type="submit"> valider </button>
+        <div class="descG"></div>
+        <div class="ligneCent"><button type="submit" class="opt"><img src="../assets/img/icon-vote.png"></button></div>
+
+
+
     </form>
+    </div>
 </div>
